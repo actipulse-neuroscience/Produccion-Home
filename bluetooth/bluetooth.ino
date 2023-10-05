b4ebfc4ee1cf6fae6b56f48c28902dc60a18ad8c
GibranEsq22 <111072705+GibranEsq22@users.noreply.github.com>
2023-10-05 11:26:39 -0600

    Codigo Produccion Home 2

---
+++ bluetooth/bluetooth.ino
@@ -0,0 +1,11 @@
+  // put your setup code here, to run once:
+#include "BluetoothSerial.h"
+BluetoothSerial SerialBT;
+void setup() {
+  // put your main code here, to run repeatedly:
+        SerialBT.begin("H-230-23-1"); // si es m
+}
+
+void loop() {
+
+}
